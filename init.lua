cd7ee516853f99e7424ac5ded2a08dce367c29de
paulrosca <paul.rosca00@e-uvt.ro>
2023-12-04 18:55:26 +0200

    Change theme to catpuccin

--- init.lua
+++ init.lua
@@ -190,14 +190,25 @@ require('lazy').setup({
     lazy = false,
     priority = 1000,
     opts = {},
+    -- config = function ()
+    --   vim.cmd.colorscheme 'tokyonight-night'
+    -- end,
   },
   {
     "ellisonleao/gruvbox.nvim",
     priority = 1000,
-    config = function ()
-      vim.cmd.colorscheme 'gruvbox'
-    end,
+    -- config = function ()
+    --   vim.cmd.colorscheme 'gruvbox'
+    -- end,
     opts = ...
+  },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function ()
+      vim.cmd.colorscheme 'catppuccin'
+    end,
   },
 
   {
