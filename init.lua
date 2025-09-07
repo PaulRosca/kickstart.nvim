9176f230cdc27e2b1484616afadcb4a2963a35ee
paulrosca <paul.rosca00@e-uvt.ro>
2025-09-07 11:40:47 +0300

    feat: go setup

--- init.lua
+++ init.lua
@@ -684,7 +684,7 @@ require('lazy').setup({
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         -- clangd = {},
-        -- gopls = {},
+        gopls = {},
         -- pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
@@ -795,6 +795,7 @@ require('lazy').setup({
         css = { 'prettierd', 'prettier', stop_after_first = true },
         yaml = { 'prettierd', 'prettier', stop_after_first = true },
         markdown = { 'prettierd', 'prettier', stop_after_first = true },
+        go = { 'goimports', 'gofmt' },
       },
     },
   },
